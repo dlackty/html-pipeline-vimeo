0387833b30158845a1a9e716b11379550aa399b1
Richard Lee <dlackty@gmail.com>
2015-11-25 02:09:45 +0800

    Avoid wrong transformations. Close #2

--- lib/html/pipeline/vimeo/vimeo_filter.rb
+++ lib/html/pipeline/vimeo/vimeo_filter.rb
@@ -16,13 +16,13 @@ module HTML
     # This filter does not write additional information to the context.
     class VimeoFilter < TextFilter
       def call
-        regex = /https?:\/\/(www.)?vimeo\.com\/([A-Za-z0-9._%-]*)((\?|#)\S+)?/
+        regex = /(\s|^)https?:\/\/(www.)?vimeo\.com\/([A-Za-z0-9._%-]*)((\?|#)\S+)?/
         @text.gsub(regex) do
-          vimeo_id = $2
+          vimeo_id = $3
           width  = context[:vimeo_width] || 440
           height = context[:vimeo_height] || 248
           show_title      = "title=0"    unless context[:vimeo_show_title]
-          show_byline     = "byline=0"   unless context[:vimeo_show_byline]  
+          show_byline     = "byline=0"   unless context[:vimeo_show_byline]
           show_portrait   = "portrait=0" unless context[:vimeo_show_portrait]
           allow_fullscreen = " webkitallowfullscreen mozallowfullscreen allowfullscreen" if context[:vimeo_allow_fullscreen]
           frameborder     = context[:vimeo_frameborder] || 0
--- spec/html/pipeline/vimeo_filter_spec.rb
+++ spec/html/pipeline/vimeo_filter_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe HTML::Pipeline::VimeoFilter do
   let(:vimeo_url) { "https://vimeo.com/137266757" }
+  let(:markdown_with_vimeo_url) { "[Vimeo](#{vimeo_url})" }
+  let(:html_with_vimeo_url) { "<a href=\"#{vimeo_url}\">Vimeo</a>)" }
+
   it "generates player iframe based on link" do
     expect(described_class.to_html(vimeo_url)).to eq(
       %(<iframe src="//player.vimeo.com/video/137266757?title=0&byline=0&portrait=0" width="440" height="248" frameborder="0"></iframe>)
     )
+  end
+
+  it "doens't transform Markdown link" do
+    expect(described_class.to_html(markdown_with_vimeo_url)).to eq(markdown_with_vimeo_url)
+  end
+
+  it "doens't transform HTML link" do
+    expect(described_class.to_html(html_with_vimeo_url)).to eq(html_with_vimeo_url)
   end
 end
